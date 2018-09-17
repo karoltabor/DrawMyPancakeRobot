1cb2f7c05145525619a11ef82b7ad6f324a80162
i389722 <i389722@student.fontys.nl>
2018-09-17 21:35:33 +0200

    Added shapes
    Added perfect triangle, heart, star and spiral, needs checking

--- xy-plot.c
+++ xy-plot.c
@@ -125,11 +125,11 @@ void moveLinear(int amountToMoveX, int amountToMoveY){
 /********************************************************
 *   Move circular
 ********************************************************/
-void moveCircular(int xCenter, int yCenter, int radius){
+void moveCircular(int xCenter, int yCenter, int radius, int finalAngle){
 	moveLinear(xCenter, yCenter-radius);
 	int angle = 0;
 	int step = 5;
-	while(angle <=360){
+	while(angle <=finalAngle){
 		int xPoint = xCenter + radius * sinDegrees(angle);
 		int yPoint = yCenter + radius * -cosDegrees(angle);
 		moveLinear(xPoint - xPosition, yPoint - yPosition);
@@ -150,6 +150,85 @@ void drawSquare(){
 	moveLinear(0,-120);
 }
 
+
+/********************************************************
+*   Draw Triangle
+********************************************************/
+void drawTriangle(int sideLength){
+	moveLinear(20,20);
+	moveLinear(sideLength,0);
+	int height = (sqrt(3)/2) * sideLength;
+	int xTop = (sideLength/2) + 20;
+	int yTop = height + 20;
+	moveLinear(xTop-xPosition,yTop-20);
+	moveLinear(20-xTop,20-yTop);
+}
+
+/********************************************************
+*   Draw Heart
+********************************************************/
+void drawHeart(int width){
+	moveLinear(20+(width/2),20);
+	//Right straight part
+	moveLinear(width/2,width/2);
+	//Right half circle of the heart
+	int rightHeartAngle = 0;
+	int xRightHeartCenter = (width*0.75)+20;
+	int yRightHeartCenter = (width/2)+20;
+	int heartRadius = width/4;
+	int heartStep = 5;
+	while(rightHeartAngle <=180){
+		int xRightHeartPoint = xRightHeartCenter + heartRadius * sinDegrees(rightHeartAngle);
+		int yRightHeartPoint = yRightHeartCenter + heartRadius * cosDegrees(rightHeartAngle);
+		moveLinear(xRightHeartPoint - xPosition, yRightHeartPoint - yPosition);
+		writeDebugStreamLine("y-point: %d, move-y: %d", yRightHeartPoint, yRightHeartPoint - yPosition);
+		rightHeartAngle += heartStep;
+	}
+	//Left half circle of the heart
+	moveCircular((width/4)+20,(width/2)+20,width/4, 180);
+	//Left straight part
+	moveLinear(width/2, -(width/2));
+}
+
+
+/********************************************************
+*   Draw Star
+********************************************************/
+void drawStar(int outerRadius, int innerRadius){
+	moveLinear(outerRadius+20,20);
+	int centerX = outerRadius + 20;
+	int centerY = outerRadius + 20;
+	int starPoints = 0;
+	while(starPoints<= 5){
+		int innerX = centerX + innerRadius * cosDegrees((starPoints*72)+36);
+		int innerY = centerY + innerRadius * sinDegrees((starPoints*72)+36);
+		moveLinear(innerX - xPosition, innerY - yPosition);
+		int outerX = centerX + outerRadius * cosDegrees(starPoints*72);
+		int outerY = centerY + outerRadius * sinDegrees(starPoints*72);
+		moveLinear(outerX - xPosition, outerY - yPosition);
+		starPoints += 1;
+	}
+}
+
+/********************************************************
+*		Draw Spiral
+********************************************************/
+void drawSpiral(int xCenter, int yCenter, double radius){
+	moveLinear(xCenter, yCenter-radius);
+	int angle = 0;
+	int step = 5;
+	while(!(xPosition = xCenter) && !(yPosition =yCenter)){
+		int xPoint = xCenter + radius * sinDegrees(angle);
+		int yPoint = yCenter + radius * -cosDegrees(angle);
+		moveLinear(xPoint - xPosition, yPoint - yPosition);
+		writeDebugStreamLine("y-point: %d, move-y: %d", yPoint, yPoint - yPosition);
+		angle += step;
+		radius -= 0.2 ;
+		if(angle = 360){
+			angle = 0;
+		}
+	}
+}
 
 /********************************************************
 *   Write the word "LEGO!"
